b6b22778daacb6853d667f85a6056061853aa049
Awortwe <enockawor@gmail.com>
2024-03-17 21:30:54 -0700

    Working on the templates

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ app.set('views', path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use('/images', express.static('images'));
+
 app.use(userRoutes);
+
+app.use(function(error, req, res, next){
+    console.log(error);
+    res.status(500).render('500');
+});
 
 db.connectToDatabase().then(function(){
     app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req,res){
-    res.send('Hello World');
+    res.redirect('/users');
 });
+
+router.get('/users', function(req,res){
+    res.render('users-list');
+})
 
 module.exports = router;
